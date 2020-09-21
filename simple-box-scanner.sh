849b37381227395c44545d3bf4ac46b097779183
fiolet <52757372+sarah-lishin@users.noreply.github.com>
2020-09-21 14:53:28 -0400

    added dirb to scans

--- simple-box-scanner.sh
+++ simple-box-scanner.sh
@@ -2,14 +2,14 @@
 
 #About:
 #Written by fiolet. 
-#A simple tool to run nmap with cve and nikto with a single command.
-#Both will run, even if no open ports are detected.
+#A simple tool to run nmap with cve, nikto, and dirb with common wordlist with a single command.
+#All will run, even if no open ports are detected.
 #Files will save to the current working directory.
 #Enjoy.
 
 #Usage:
 #Run from the command line: ./simple-box-scanner <IP> <Box Name>
-#I recommend adding an alias to your ~./bashrc file
+#I recommend adding a alias to your ~./bashrc file
 
 ports="80 443"
 old="$IFS"
@@ -19,8 +19,11 @@ IFS=$old
 
 var1="nmap_"
 var2="nikto_"
+var3="dirb_"
 nmap="$var1$box"
 nikto="$var2$box"
+dirb="$var3$box"
 
 nmap -Pn -n -sV --script vuln $1 | tee $nmap
 nikto -host $1 | tee $nikto
+dirb http://$1 | tee $dirb
