9db064cc78b12a284171be270031304f360f48e2
fiolet <52757372+sarah-lishin@users.noreply.github.com>
2020-09-28 15:23:32 -0400

    added wpscan and enum4linux

--- simple-box-scanner.sh
+++ simple-box-scanner.sh
@@ -20,10 +20,17 @@ IFS=$old
 var1="nmap_"
 var2="nikto_"
 var3="dirb_"
+var4="wpscan_"
+var5="enum4linux_"
+
 nmap="$var1$box"
 nikto="$var2$box"
 dirb="$var3$box"
+wpscan="$var4$box"
+enum4linux="$var5$box"
 
 nmap -Pn -n -sV --script vuln $1 | tee $nmap
 nikto -host $1 | tee $nikto
 dirb http://$1 | tee $dirb
+wpscan --url $1 | tee $wpscan
+enum4linux $1 | tee $enum4linux
